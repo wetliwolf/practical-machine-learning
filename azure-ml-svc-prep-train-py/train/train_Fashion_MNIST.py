3ba1228c374b447aa17010afe41603c0f60f26df
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-01-29 19:14:56 +0000

    chore(package): update sinon to version 4.0.0

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -10,4 +10,12 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.lay
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import backend as K
+
+import azureml.core
+from azureml.core.run import Run
+
+print("TensorFlow version:", tf.__version_
