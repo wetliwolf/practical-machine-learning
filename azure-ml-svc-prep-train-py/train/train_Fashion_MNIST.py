4916df4bb0ba93dbfcc7cfcd562549b0fc9cb6c7
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-01-29 09:39:00 +0000

    fixed timer

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -1,3 +1,13 @@
 from __future__ import print_function
 import warnings
-w
+warnings.filterwarnings('ignore')
+
+import os
+import numpy as np
+from functools import partial
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.lay
