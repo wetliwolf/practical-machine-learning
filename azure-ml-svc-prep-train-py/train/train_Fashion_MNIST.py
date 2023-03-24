29c3fd416883a43f435e0ea1e665fda4d14cd71d
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-03-24 08:04:36 +0000

    ignore file

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -65,4 +65,9 @@ img_index = 1
 plt.imsave('fashion.png', 1-x_train[img_index][:, :, 0], cmap='gray')
 run.log_image('Fashion Sample', path='fashion.png')
 
-print
+print("Before:\n{}".format(y_train[:4]))
+# convert class vectors to binary class matrices.  One-hot encoding
+#  3 => 0 0 0 1 0 0 0 0 0 0 and 1 => 0 1 0 0 0 0 0 0 0 0 
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print("
