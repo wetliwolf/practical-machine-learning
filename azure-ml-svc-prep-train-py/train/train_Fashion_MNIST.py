c26821050915a300134a2bc9789e0855b422d8d7
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-04-07 14:06:37 +0000

    Moving things around

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -96,4 +96,10 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 #   Define callbacks
 my_callbacks = [
-    Earl
+    EarlyStopping(monitor='val_accuracy', patience=5, mode='max'), 
+    ModelCheckpoint('./outputs/checkpoint.h5', verbose=1)
+]
+
+#   Train the model and test/validate the mode with the test data after each cycle (epoch) through the training data
+#   Return history of loss and accuracy for each epoch
+hist = mode
