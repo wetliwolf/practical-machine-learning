64e6c3e7630a9f0622e70ca7b96af8fa1e21b275
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-01-15 09:35:31 +0000

    1.0.40

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -4,4 +4,10 @@ IFS=$'\n\t'
 
 # -e: immediately exit if any command has a non-zero exit status
 # -o: prevents errors in a pipeline from being masked
-# IFS new va
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+dec
