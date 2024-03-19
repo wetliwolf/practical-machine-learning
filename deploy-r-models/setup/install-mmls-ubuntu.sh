9c485cc4759cb4627486afa40f2175b09a9f27a7
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-03-19 17:07:01 +0000

    1.4.7

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -13,4 +13,13 @@ shift $((OPTIND-1))
 
 if [[ -z "$password" ]]; then
 	while : ; do
-		ec
+		echo -n "Enter a password for the Machine Learning Server admin:"
+		read -s password
+		echo
+		echo -n "Please repeat the password for the Machine Learning Server admin:"
+		read -s password_confirm
+		echo
+		if [[ "$password" == "$password_confirm" ]]; then
+			break
+		else
+			echo "The passwords do not 
